51fd9abad6ba246b40ba27e90ec8e1d06d3741f5
Petar <p.hrusanov@gmail.com>
2020-03-26 00:06:32 +0200

    Minor Model and Finalising Improvements

---
+++ Data/HealthAssistApp.Data/Migrations/20200325220344_PartOfMeal.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HealthAssistApp.Data.Migrations
+{
+    public partial class PartOfMeal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PartOfMeal",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PartOfMeal",
+                table: "Recipes");
+        }
+    }
+}
