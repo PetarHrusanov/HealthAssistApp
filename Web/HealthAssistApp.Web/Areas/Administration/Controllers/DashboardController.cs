d2d8175a95e68ec7a4bb9ceed4900297ab7cd564
Petar <p.hrusanov@gmail.com>
2020-04-11 14:45:19 +0300

    Prototype Admin Diseases & Recipes

--- Web/HealthAssistApp.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Web/HealthAssistApp.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,23 +1,41 @@
-﻿namespace HealthAssistApp.Web.Areas.Administration.Controllers
+﻿// <copyright file="DashboardController.cs" company="HealthAssistApp">
+// Copyright (c) HealthAssistApp. All Rights Reserved.
+// </copyright>
+
+namespace HealthAssistApp.Web.Areas.Administration.Controllers
 {
+    using System.Threading.Tasks;
+    using HealthAssistApp.Data;
     using HealthAssistApp.Services.Data;
     using HealthAssistApp.Web.ViewModels.Administration.Dashboard;
 
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
 
     public class DashboardController : AdministrationController
     {
         private readonly ISettingsService settingsService;
 
-        public DashboardController(ISettingsService settingsService)
+        private readonly ApplicationDbContext db;
+
+        public DashboardController(ApplicationDbContext db)
         {
-            this.settingsService = settingsService;
+            this.db = db;
         }
 
         public IActionResult Index()
         {
-            var viewModel = new IndexViewModel { SettingsCount = this.settingsService.GetCount(), };
-            return this.View(viewModel);
+            return this.View();
+        }
+
+        public async Task<IActionResult> Diseases()
+        {
+            return View(await db.Diseases.ToListAsync());
+        }
+
+        public async Task<IActionResult> Recipes()
+        {
+            return View(await db.Recipes.ToListAsync());
         }
     }
 }
